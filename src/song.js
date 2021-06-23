245cd0b2c3c106503f70a876145ab5a2e220f1ca
kkirby16 <kkirby16@my.whitworth.edu>
2021-06-23 00:17:55 -0700

    create a constructor in the Song class that pushes each new instance of a song into an empty array.

--- src/song.js
+++ src/song.js
@@ -1,1 +1,23 @@
-class Song {}
+class Song {
+  constructor(song, songAttributes) {
+    this.id = song.id;
+    this.name = songAttributes.name; //the attributes can be called off of the songAttributes variable
+    this.artist = songAttributes.artist;
+    this.album = songAttributes.album;
+    this.song_url = songAttributes.song_url;
+    this.submitted_by = songAttributes.submitted_by;
+    this.genre = songAttributes.genre;
+    Song.all.push(this); //with each new instance, I'll be pushing this which represents the current instance that was created into Song.all
+    debugger;
+    //Song.all is a good name that develpers have decided to use when talking about an array of something.
+  }
+}
+
+Song.all = []; //this array is in the global scope and needs to be in the global scope outside of the song class so it can even know about the song class.
+//our contructor needs to be correct
+//constructor will give us all of our attributes so we can use copies or prototypes of our song object.
+
+//why we want a song class: if we wanted to have an edit button to be able to render an edit form and edit songs we would need to know what song/object we were clicking on. without data ids and without some kind of oojs class that is encapsulating all this data you'd have to find that object and then update it, whereas in oojs I can have an array of javascript instances and use that instead to find this specific instance and update it.
+
+//if we didn't have data-ids on our divs it would be very cumbersome to know which song we clicked for when we clicked a song's edit button. we'd have to find a song one by one.
+//to get rid of that issue, we can use object orientation where our objects can store all the data about a particular song in one spot.
