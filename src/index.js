8742d43118d38a1cb512bfd7e728fab0f33c69ed
kkirby16 <kkirby16@my.whitworth.edu>
2022-02-19 00:06:24 -0800

    add console log in second then statement in post fetch function

--- src/index.js
+++ src/index.js
@@ -70,8 +70,9 @@ function postFetch(name, artist, album, song_url, submitted_by, genre_id) {
     //this will send back the data to my api.
     //the keys in our body data have to be exactly what they are like in the database.
   })
-    .then((response) => console.log(response.json()))
+    .then((response) => response.json())
     .then((song) => {
+      console.log("what is song?", song);
       let newSong = new Song(song, song.attributes);
       let songsContainer = document.querySelector("#song-container");
       let songsContainerTitle = document.querySelector("#song-container-title");
